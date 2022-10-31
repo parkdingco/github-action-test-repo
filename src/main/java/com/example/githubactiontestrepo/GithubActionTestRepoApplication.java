a99398b25d0eb47bb2c9f3cdb332019897a2294e
jypark <jypark@coway.com>
2022-10-31 16:15:34 +0900

    first commit

---
+++ src/main/java/com/example/githubactiontestrepo/GithubActionTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactiontestrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionTestRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionTestRepoApplication.class, args);
+    }
+
+}
