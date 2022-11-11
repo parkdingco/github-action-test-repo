feada5a2ea8a11def80b24d4e7413ca123f31824
jypark <jypark@coway.com>
2022-11-11 10:52:32 +0900

    aa

--- src/main/java/com/example/githubactiontestrepo/GithubActionTestRepoApplication.java
+++ src/main/java/com/example/githubactiontestrepo/GithubActionTestRepoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubActionTestRepoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GithubActionTestRepoApplication.class, args);
     }
-
 }
